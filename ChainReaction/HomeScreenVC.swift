f2eab5f54f4a4f9aa13b5ac22981786694532c7b
imtiaz191919 <imtiaz@uncommon.is>
2019-08-21 00:35:45 +0530

    initial commit with pods

---
+++ ChainReaction/HomeScreenVC.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  HomeScreenVC
+//
+//  Created by imtiaz abbas on 21/08/19.
+//  Copyright © 2019 imtiaz abbas. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenVC: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
---
+++ Podfile
@@ -0,0 +1,5 @@
+target 'ChainReaction' do
+    use_frameworks!
+    pod 'SwiftLint'
+    pod 'SteviaLayout'
+end
